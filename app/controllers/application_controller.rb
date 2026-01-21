3d33c09c47edd525b03041f079f18dc66ba6fa93
KauanNasciment0 <kauan.silvanascimento@outlook.com>
2026-01-21 20:19:31 -0300

    Adição do metodo require_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,17 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
+
+  def require_admin
+    if !logged_in?
+      flash[:alert] = "Você precisa estar logado para acessar esta página."
+      redirect_to login_path and return
+
+      elsif !current_user.admin?
+       flash[:alert] = "Acesso restrito a administradores."
+        redirect_to root_path and return
+
+    end
+  end
+
 end
