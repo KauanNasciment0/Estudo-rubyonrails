362b31e727492e9e91ee3568ba7219e7b05f33f2
KauanNasciment0 <kauan.silvanascimento@outlook.com>
2026-01-21 20:34:00 -0300

    Ajustes no routes.rb para evitar repetição e adicionar novas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,7 @@ Rails.application.routes.draw do
   # Rotas de Usuário
   get 'cadastro', to: 'users#new', as: :signup
 
-  # Aqui está o segredo: Adicionamos o :index na lista!
-  # Isso cria automaticamente o GET /users apontando para users#index
   resources :users, only: [:create, :index]
 
-  # Saúde do sistema
   get "up" => "rails/health#show", as: :rails_health_check
 end
