d7a2c795214a04916f2095710dbd51c7834af9a4
KauanNasciment0 <kauan.silvanascimento@outlook.com>
2026-01-22 00:18:15 -0300

    fix: corrige verbo HTTP no teste de create e ajusta estilo com rubocop

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get users_new_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should post create" do
